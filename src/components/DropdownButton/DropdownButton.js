704895e99c5f556e7c0a4866ce9fff62c1a72821
leonelborgesdev <leonel03t@gmail.com>
2022-09-09 23:14:12 -0400

    dropdown

--- src/components/DropdownButton/DropdownButton.js
+++ src/components/DropdownButton/DropdownButton.js
@@ -5,15 +5,25 @@ import "./DropdownButton.css";
 export const DropdownButton = () => {
   const { dropdowname } = useSelector((state) => state);
   return (
-    <div>
+    <div className="header_lista">
       <ul className="lista_select">
-        <li>{dropdowname}</li>
-        <ul>
-          <li>Razon Social</li>
-          <li>Nit</li>
-          <li>Telefono</li>
-          <li>Codigo</li>
-        </ul>
+        <li>
+          <h3>Nombre</h3>
+          <ul>
+            <li>
+              <h3>Razon Social</h3>
+            </li>
+            <li>
+              <h3>Nit</h3>
+            </li>
+            <li>
+              <h3>Telefono</h3>
+            </li>
+            <li>
+              <h3>Codogo</h3>
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
